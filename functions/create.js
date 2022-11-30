d98f2d6244a3183f686abef8dfbda414248a9050
Dani <dan.avila7@gmail.com>
2022-11-29 19:44:07 -0500

    dev,prod,params and datadog

--- functions/create.js
+++ functions/create.js
@@ -1,12 +1,30 @@
 const { v4 } = require('uuid')
+const AWS = requre('aws-sdk')
 
 const createProduct = async(event) => {
 
-  const { name, price } = event.body;
+  const dynamodb = new AWS.Dynamodb.DocumentClient();
+  const { name, price } = JSON.parse(event.body);
   const createAt = new Date();
   const id = v4();
+  const TABLE_NAME = process.env.DYNAMODB_PRODUCT_TABLE
 
+  const newProduct = {
+    id,
+    name,
+    price,
+    createdAt
+  }
 
+  await dynamodb.put({
+    TableName: TABLE_NAME,
+    Item: newProduct
+  }).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(newProduct)
+  }
 
 }
 
